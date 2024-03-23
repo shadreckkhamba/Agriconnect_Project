05c0f655820ec3c5dd9031bc4aa48fd9cc3d67bc
Ben Kaponda <bsc-inf-02-19@unima.ac.mw>
2024-03-23 10:52:13 -0700

    index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php require_once('header.php'); ?>
+
+<?php
+$statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
+$statement->execute();
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach ($result as $row)
+{
+    $cta_title = $row['cta_title'];
+    $cta_content = $row['cta_content'];
+    $cta_read_more_text = $row['cta_read_more_text'];
+    $cta_read_more_url = $row['cta_read_more_url'];
+    $cta_photo = $row['cta_photo'];
+    $featured_product_title = $row['featured_product_title'];
+    $featured_product_subtitle = $row['featured_product_subtitle'];
+    $latest_product_title = $row['latest_product_title'];
+    $latest_product_subtitle = $row['latest_product_subtitle'];
+    $popular_product_title = $row['popular_product_title'];
+    $popular_product_subtitle = $row['popular_product_subtitle'];
+    $total_featured_product_home = $row['total_featured_product_home'];
+    $total_latest_product_home = $row['total_latest_product_home'];
+    $total_popular_product_home = $row['total_popular_product_home'];
+    $home_service_on_off = $row['home_service_on_off'];
+    $home_welcome_on_off = $row['home_welcome_on_off'];
+    $home_featured_product_on_off = $row['home_featured_product_on_off'];
+    $home_latest_product_on_off = $row['home_latest_product_on_off'];
+    $home_popular_product_on_off = $row['home_popular_product_on_off'];
+
+}
