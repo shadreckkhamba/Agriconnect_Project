c25f1a0ddf9879a2b54d009f5b87f8d60c0193d1
shadreck khamba <Bsc-inf-21-19@unima.ac.mw>
2024-03-26 01:32:26 -0700

    update header

--- header.php
+++ header.php
@@ -11,3 +11,67 @@ $error_message = '';
 $success_message = '';
 $error_message1 = '';
 $success_message1 = '';
+
+// Getting all language variables into array as global variable
+$i=1;
+$statement = $pdo->prepare("SELECT * FROM tbl_language");
+$statement->execute();
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
+foreach ($result as $row) {
+	define('LANG_VALUE_'.$i,$row['lang_value']);
+	$i++;
+}
+
+$statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
+$statement->execute();
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach ($result as $row)
+{
+	$logo = $row['logo'];
+	$favicon = $row['favicon'];
+	$contact_email = $row['contact_email'];
+	$contact_phone = $row['contact_phone'];
+	$meta_title_home = $row['meta_title_home'];
+    $meta_keyword_home = $row['meta_keyword_home'];
+    $meta_description_home = $row['meta_description_home'];
+    $before_head = $row['before_head'];
+    $after_body = $row['after_body'];
+}
+
+// Checking the order table and removing the pending transaction that are 24 hours+ old. Very important
+$current_date_time = date('Y-m-d H:i:s');
+$statement = $pdo->prepare("SELECT * FROM tbl_payment WHERE payment_status=?");
+$statement->execute(array('Pending'));
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
+foreach ($result as $row) {
+	$ts1 = strtotime($row['payment_date']);
+	$ts2 = strtotime($current_date_time);     
+	$diff = $ts2 - $ts1;
+	$time = $diff/(3600);
+	if($time>24) {
+
+		// Return back the stock amount
+		$statement1 = $pdo->prepare("SELECT * FROM tbl_order WHERE payment_id=?");
+		$statement1->execute(array($row['payment_id']));
+		$result1 = $statement1->fetchAll(PDO::FETCH_ASSOC);
+		foreach ($result1 as $row1) {
+			$statement2 = $pdo->prepare("SELECT * FROM tbl_product WHERE p_id=?");
+			$statement2->execute(array($row1['product_id']));
+			$result2 = $statement2->fetchAll(PDO::FETCH_ASSOC);							
+			foreach ($result2 as $row2) {
+				$p_qty = $row2['p_qty'];
+			}
+			$final = $p_qty+$row1['quantity'];
+
+			$statement = $pdo->prepare("UPDATE tbl_product SET p_qty=? WHERE p_id=?");
+			$statement->execute(array($final,$row1['product_id']));
+		}
+		
+		// Deleting data from table
+		$statement1 = $pdo->prepare("DELETE FROM tbl_order WHERE payment_id=?");
+		$statement1->execute(array($row['payment_id']));
+
+		$statement1 = $pdo->prepare("DELETE FROM tbl_payment WHERE id=?");
+		$statement1->execute(array($row['id']));
+	}
+}
