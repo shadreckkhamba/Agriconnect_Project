af22ae22b29582f4eadae6f8a93deeb4f647eab4
PatrickKamphulusa <bsc-inf-12-19@unima.ac.mw>
2024-04-13 17:23:37 +0300

    creating the actual form for filling the login creditials

--- login.php
+++ login.php
@@ -65,3 +65,51 @@ if($total==0 && $total2==0) {
 }
 }
 ?>
+
+<div class="page-banner" style="background-color:#444;background-image: url(assets/uploads/<?php echo $banner_login; ?>);">
+    <div class="inner">
+        <h1><?php echo LANG_VALUE_10; ?></h1>
+    </div>
+</div>
+
+<div class="page">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="user-content">
+                      <form action="" method="post">
+                        <?php $csrf->echoInputField(); ?>                  
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                                <?php
+                                if($error_message != '') {
+                                    echo "<div class='error' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$error_message."</div>";
+                                }
+                                if($success_message != '') {
+                                    echo "<div class='success' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$success_message."</div>";
+                                }
+                                ?>
+                                <div class="form-group">
+                                    <label for=""><?php echo LANG_VALUE_94; ?> *</label>
+                                    <input type="email" class="form-control" name="cust_email">
+                                </div>
+                                <div class="form-group">
+                                    <label for=""><?php echo LANG_VALUE_96; ?> *</label>
+                                    <input type="password" class="form-control" name="cust_password">
+                                </div>
+                                <div class="form-group">
+                                    <label for=""></label>
+                                    <input type="submit" class="btn btn-success" value="<?php echo LANG_VALUE_4; ?>" name="form1">
+                                </div>
+                                <a href="forget-password.php" style="color:#e4144d;"><?php echo LANG_VALUE_97; ?>?</a>
+                            </div>
+                        </div>                        
+                    </form>
+                </div>                
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once('footer.php'); 
