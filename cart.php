dda24d474253d8aae1b38c0dd6801c9ccdc0badd
PatrickKamphulusa <bsc-inf-12-19@unima.ac.mw>
2024-04-18 08:48:14 +0300

    adding logic to add up the total amount the products in cart and displaying products in table

--- cart.php
+++ cart.php
@@ -63,3 +63,207 @@ if(isset($_POST['form1'])){
             <?php
 }
 ?>
+
+<div class="page-banner" style="background-image: url(assets/uploads/<?php echo $banner_cart; ?>)">
+    <div class="overlay"></div>
+    <div class="page-banner-inner">
+        <h1><?php echo LANG_VALUE_18; ?></h1>
+</div>
+</div>
+
+<div class="page">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+            <?php if(!isset($_SESSION['cart_p_id'])): ?>
+                <?php echo '<h2 class="text-center">Cart is Empty!!</h2></br>'; ?>
+                <?php echo '<h4 class="text-center">Add products to the cart in order to view it here.</h4>'; ?>
+            <?php else: ?>
+                <form action="" method="post">
+                    <?php $csrf->echoInputField(); ?>
+                    <div class="cart">
+                        <table class="table table-responsive table-hover table-bordered">
+                            <tr>
+                                <th><?php echo '#' ?></th>
+                                <th><?php echo LANG_VALUE_8; ?></th>
+                                <th><?php echo LANG_VALUE_47; ?></th>
+                                <th><?php echo LANG_VALUE_159; ?></th>
+                                <th class="text-right"><?php echo LANG_VALUE_82; ?></th>
+                                <th class="text-center" style="width: 100px;"><?php echo LANG_VALUE_83; ?></th>
+            </tr>
+
+            <?php
+                        $table_total_price = 0;
+
+                        $i=0;
+                        foreach($_SESSION['cart_p_id'] as $key => $value) 
+                        {
+                            $i++;
+                            $arr_cart_p_id[$i] = $value;
+                        }
+
+                        $i=0;
+                        foreach($_SESSION['cart_size_id'] as $key => $value) 
+                        {
+                            $i++;
+                            $arr_cart_size_id[$i] = $value;
+                        }
+
+                        $i=0;
+                        foreach($_SESSION['cart_size_name'] as $key => $value) 
+                        {
+                            $i++;
+                            $arr_cart_size_name[$i] = $value;
+                        }
+
+                        $i=0;
+                        foreach($_SESSION['cart_color_id'] as $key => $value) 
+                        {
+                            $i++;
+                            $arr_cart_color_id[$i] = $value;
+                        }
+
+                        $i=0;
+                        foreach($_SESSION['cart_color_name'] as $key => $value) 
+                        {
+                            $i++;
+                            $arr_cart_color_name[$i] = $value;
+                        }
+
+                        $i=0;
+                        foreach($_SESSION['cart_p_qty'] as $key => $value) 
+                        {
+                            $i++;
+                            $arr_cart_p_qty[$i] = $value;
+                        }
+
+                        $i=0;
+                        foreach($_SESSION['cart_p_current_price'] as $key => $value) 
+                        {
+                            $i++;
+                            $arr_cart_p_current_price[$i] = $value;
+                        }
+
+                        $i=0;
+                        foreach($_SESSION['cart_p_name'] as $key => $value) 
+                        {
+                            $i++;
+                            $arr_cart_p_name[$i] = $value;
+                        }
+
+                        $i=0;
+                        foreach($_SESSION['cart_p_featured_photo'] as $key => $value) 
+                        {
+                            $i++;
+                            $arr_cart_p_featured_photo[$i] = $value;
+                        }
+                        ?>
+                        <?php for($i=1;$i<=count($arr_cart_p_id);$i++): ?>
+                        <tr>
+                            <td><?php echo $i; ?></td>
+                            <td>
+                                <img src="assets/uploads/<?php echo $arr_cart_p_featured_photo[$i]; ?>" alt="">
+                            </td>
+                            <td><?php echo $arr_cart_p_name[$i]; ?></td>
+                            <td><?php echo "MWK "; ?><?php echo $arr_cart_p_current_price[$i]; ?></td>
+                            <td>
+                                <input type="hidden" name="product_id[]" value="<?php echo $arr_cart_p_id[$i]; ?>">
+                                <input type="hidden" name="product_name[]" value="<?php echo $arr_cart_p_name[$i]; ?>">
+                                <input type="number" class="input-text qty text" step="1" min="1" max="" name="quantity[]" value="<?php echo $arr_cart_p_qty[$i]; ?>" title="Qty" size="4" pattern="[0-9]*" inputmode="numeric">
+                            </td>
+                            <td class="text-right">
+                                <?php
+                                $row_total_price = $arr_cart_p_current_price[$i]*$arr_cart_p_qty[$i];
+                                $table_total_price = $table_total_price + $row_total_price;
+                                ?>
+                                <?php echo "MWK "; ?><?php echo $row_total_price; ?>
+                            </td>
+                            <td class="text-center">
+                                <a onclick="return confirmDelete();" href="cart-item-delete.php?id=<?php echo $arr_cart_p_id[$i]; ?>&size=<?php echo $arr_cart_size_id[$i]; ?>&color=<?php echo $arr_cart_color_id[$i]; ?>" class="trash"><i class="fa fa-trash" style="color:red;"></i></a>
+                            </td>
+                        </tr>
+                        <?php endfor; ?>
+                        <tr>
+                            <th colspan="5" class="total-text">Total</th>
+                            <th class="total-amount"><?php echo "MWK "; ?><?php echo $table_total_price; ?></th>
+                            <th></th>
+                        </tr>
+                    </table> 
+                </div>
+
+                <div class="cart-buttons">
+                    <ul>
+                        <li><input type="submit" value="<?php echo LANG_VALUE_20; ?>" class="btn btn-primary" name="form1"></li>
+                        <li><a href="index.php" class="btn btn-primary"><?php echo LANG_VALUE_85; ?></a></li>
+                        <li><a href="checkout.php" class="btn btn-primary"><?php echo LANG_VALUE_23; ?></a></li>
+                    </ul>
+                </div>
+                </form>
+                <?php endif; ?>
+
+                
+
+			</div>
+		</div>
+	</div>
+</div>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body, html {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+}
+
+footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+
+}
+    </style>
+</head>
+<body>
+
+    <footer>
+        <?php require_once('footer.php'); ?>
+    </footer>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
