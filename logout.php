53ea511b46822029482f3cd27155ea89db9a6330
PatrickKamphulusa <bsc-inf-12-19@unima.ac.mw>
2024-04-26 19:57:56 +0300

    commenting the code

--- logout.php
+++ logout.php
@@ -1,7 +1,10 @@
 <?php
 ob_start();
+//starting session
 session_start();
+//specify the path where the configuration file is
 include 'admin/inc/config.php';
 unset($_SESSION['customer']);
+//redirect user to login page after logging out
 header("location: ".BASE_URL.'login.php);
 ?>
