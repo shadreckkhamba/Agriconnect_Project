269f78b839942b008fabb359d0f9f9e7d1612fcf
PatrickKamphulusa <bsc-inf-12-19@unima.ac.mw>
2024-04-28 09:02:10 +0300

    add a file for options of updating cusomer profile, view orders,update password, and shipping information

---
+++ customer-sidebar.php
@@ -0,0 +1,15 @@
+<div class = "user-sidebar">
+    <!-- unordered list -->
+    <ul>
+        <!-- button to take you to a form to update profile -->
+        <a href = "customer-profile-update.php"><button class = "btn btn-danger"><?php echo LANG_VALUE_117; ?></button></a>
+        <!-- button to take you to a form to update billing and shipping information -->
+        <a href = "customer-billing-shipping-update.php"><button class="btn btn-danger"><?php echo "Update Shipping Information"; ?></button></a>
+        <!-- button to take you to a form to update password -->
+        <a href = "customer-password-update.php"><button class="btn btn-danger"><?php echo LANG_VALUE_99; ?></button></a>
+        <!-- button to take you to view orders or order history -->
+        <a href = "customer-order.php"><button class="btn btn-danger"><?php echo LANG_VALUE_24; ?></button></a>
+        <!-- button to allow customer to logout -->
+        <a href = "logout.php"><button class="btn btn-danger"><?php echo LANG_VALUE_14; ?></button></a>
+    </ul>
+</div>
