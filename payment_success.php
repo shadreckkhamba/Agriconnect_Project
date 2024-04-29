95191c542fb5b9aa0b4e897c0b14bdc27ad60f2f
PatrickKamphulusa <bsc-inf-12-19@unima.ac.mw>
2024-04-29 17:36:07 +0300

    crating payment_success file

---
+++ payment_success.php
@@ -0,0 +1,8 @@
+<?php require_once('header.php'); ?>
+
+<div class = "page">
+    <div class = "container">
+        <div class = "row">
+            <div class = "col-md-12">
+                <p>
+                    <h3 style
