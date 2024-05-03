9525873ae1ca7e9ce03d4ef2d22b921fa7615a84
Patrick Kamphulusa <bsc-inf-12-19@unima.ac.mw>
2024-05-03 11:57:35 +0300

    the configuration file that hold database details to allow connection

---
+++ vendor/inc/config.php
@@ -0,0 +1,34 @@
+<?php
+// Error Reporting Turn On
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+
+// Host Name
+$dbhost = 'localhost';
+
+// Database Name
+$dbname = 'agriconnect';
+
+// Database Username
+$dbuser = 'root';
+
+// Database Password
+$dbpass = '';
+
+// Defining base url
+define("BASE_URL", "");
+
+//port number
+$port = '3306';
+
+// Getting Admin url
+define("ADMIN_URL", BASE_URL . "admin" . "/");
+
+try {
+	$pdo = new PDO("mysql:host={$dbhost};port={$port};dbname={$dbname}", $dbuser, $dbpass);
+	$conn=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname,$port);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch( PDOException $exception ) {
+	echo "Connection error :" . $exception->getMessage();
+}
