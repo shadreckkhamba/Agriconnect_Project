e04dcaf6187673dbd43b86cd7d4d0a5659d921a0
benkaponda10 <bsc-inf-02-19@unima.ac.mw>
2024-05-04 03:48:21 -0700

    modification

--- header.php
+++ header.php
@@ -353,6 +353,7 @@ if($cur_page == 'customer-order.php') {
 						<?php
 						$statement = $pdo->prepare("SELECT * FROM tbl_page WHERE id=1");
 						$statement->execute();
+						
 						$result = $statement->fetchAll(PDO::FETCH_ASSOC);		
 						foreach ($result as $row) {
 							$about_title = $row['about_title'];
