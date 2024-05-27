8967d6ab9bc5dbefbc8932f3d0a8be04505e35e5
bsc-inf-12-19 <bsc-inf-12-19@unima.ac.mw>
2024-05-27 13:38:31 +0300

    create and implementing delete logic for vendors

---
+++ admin/vendor-delete.php
@@ -0,0 +1,37 @@
+<?php require_once('header.php'); ?>
+
+<?php
+if(!isset($_REQUEST['id'])) {
+	header('location: logout.php');
+	exit;
+} else {
+	// Check the id is valid or not
+	$statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_id=?");
+	$statement->execute(array($_REQUEST['id']));
+	$total = $statement->rowCount();
+	if( $total == 0 ) {
+		header('location: logout.php');
+		exit;
+	}
+
+	foreach($statement as $row){
+		$cust_email= $row['cust_email'];
+		$statement1 = $pdo->prepare("DELETE FROM tbl_user WHERE email=?");
+		$statement1->execute(array($cust_email));
+	}
+
+}
+?>
+
+<?php
+
+	// Delete from tbl_customer
+	$statement = $pdo->prepare("DELETE FROM tbl_customer WHERE cust_id=?");
+	$statement->execute(array($_REQUEST['id']));
+
+	// Delete from tbl_rating
+	$statement = $pdo->prepare("DELETE FROM tbl_rating WHERE cust_id=?");
+	$statement->execute(array($_REQUEST['id']));
+
+	header('location: vendor.php');
+?>
