7db8a30a58e48e2836b722efd2179bcf31cdebeb
bsc-inf-12-19 <bsc-inf-12-19@unima.ac.mw>
2024-05-28 16:14:33 +0300

    implement to retrieve order information from the database

---
+++ admin/order.php
@@ -0,0 +1,98 @@
+<?php require_once('header.php'); ?>
+
+<?php
+$error_message = '';
+if(isset($_POST['form1'])) {
+    $valid = 1;
+    if(empty($_POST['subject_text'])) {
+        $valid = 0;
+        $error_message .= 'Subject can not be empty\n';
+    }
+    if(empty($_POST['message_text'])) {
+        $valid = 0;
+        $error_message .= 'Subject can not be empty\n';
+    }
+    if($valid == 1) {
+
+        $subject_text = strip_tags($_POST['subject_text']);
+        $message_text = strip_tags($_POST['message_text']);
+
+        // Getting Customer Email Address
+        $statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_id=?");
+        $statement->execute(array($_POST['cust_id']));
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+        foreach ($result as $row) {
+            $cust_email = $row['cust_email'];
+        }
+
+        // Getting Admin Email Address
+        $statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+        foreach ($result as $row) {
+            $admin_email = $row['contact_email'];
+        }
+
+        $order_detail = '';
+        $statement = $pdo->prepare("SELECT * FROM tbl_payment WHERE payment_id=?");
+        $statement->execute(array($_POST['payment_id']));
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+        foreach ($result as $row) {
+        	if($row['payment_method'] == 'Bank Deposit'):
+				$payment_details = '
+Transaction Details: <br>'.$row['bank_transaction_info'];
+        	endif;
+
+            $order_detail .= '
+Customer Name: '.$row['customer_name'].'<br>
+Customer Email: '.$row['customer_email'].'<br>
+Payment Method: '.$row['payment_method'].'<br>
+Payment Date: '.$row['payment_date'].'<br>
+Payment Details: <br>'.$payment_details.'<br>
+Paid Amount: '.$row['paid_amount'].'<br>
+Payment Status: '.$row['payment_status'].'<br>
+Shipping Status: '.$row['shipping_status'].'<br>
+Payment Id: '.$row['payment_id'].'<br>
+            ';
+        }
+
+        $i=0;
+        $statement = $pdo->prepare("SELECT * FROM tbl_order WHERE payment_id=?");
+        $statement->execute(array($_POST['payment_id']));
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+        foreach ($result as $row) {
+            $i++;
+            $order_detail .= '
+<br><b><u>Product Item '.$i.'</u></b><br>
+Product Name: '.$row['product_name'].'<br>
+Size: '.$row['size'].'<br>
+Color: '.$row['color'].'<br>
+Quantity: '.$row['quantity'].'<br>
+Unit Price: '.$row['unit_price'].'<br>
+            ';
+        }
+
+        $statement = $pdo->prepare("INSERT INTO tbl_customer_message (subject,message,order_detail,cust_id) VALUES (?,?,?,?)");
+        $statement->execute(array($subject_text,$message_text,$order_detail,$_POST['cust_id']));
+
+        // sending email
+        $to_customer = $cust_email;
+        $message = '
+<html><body>
+<h3>Message: </h3>
+'.$message_text.'
+<h3>Order Details: </h3>
+'.$order_detail.'
+</body></html>
+';
+
+}
+?>
+<?php
+if($error_message != '') {
+    echo "<script>alert('".$error_message."')</script>";
+}
+if($success_message != '') {
+    echo "<script>alert('".$success_message."')</script>";
+}
+?>
