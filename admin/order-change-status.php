0d0aa4b21d88416070ecb3fa1d250b0d9863e99d
bsc-inf-12-19 <bsc-inf-12-19@unima.ac.mw>
2024-05-31 17:52:01 +0300

    code for changing the status of the order if it has been paid for

---
+++ admin/order-change-status.php
@@ -0,0 +1,23 @@
+<?php require_once('header.php'); ?>
+
+<?php
+if( !isset($_REQUEST['id']) || !isset($_REQUEST['task']) ) {
+	header('location: logout.php');
+	exit;
+} else {
+	// Check the id is valid or not
+	$statement = $pdo->prepare("SELECT * FROM tbl_payment WHERE id=?");
+	$statement->execute(array($_REQUEST['id']));
+	$total = $statement->rowCount();
+	if( $total == 0 ) {
+		header('location: logout.php');
+		exit;
+	}
+}
+?>
+<?php
+    // changing the status of the order in the database
+	$statement = $pdo->prepare("UPDATE tbl_payment SET payment_status=? WHERE id=?");
+	$statement->execute(array($_REQUEST['task'],$_REQUEST['id']));
+	header('location: order.php');
+?>
