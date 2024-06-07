04d2affaaa8ac6715f2c209d5d578e899606b6c1
shadreck khamba <Bsc-inf-21-19@unima.ac.mw>
2024-06-07 15:01:59 -0700

    modified service delete to ensure that both the service record and its associated photo are properly deleted from the database and the file system

--- admin/service-delete.php
+++ admin/service-delete.php
@@ -1,39 +1,44 @@
 <?php require_once('header.php'); ?>
 
 <?php
+// Check if an ID is provided in the request
 if(!isset($_REQUEST['id'])) {
-	header('location: logout.php');
-	exit;
+    // Redirect to logout if ID is not set
+    header('location: logout.php');
+    exit;
 } else {
-	// Check the id is valid or not
-	$statement = $pdo->prepare("SELECT * FROM tbl_service WHERE id=?");
-	$statement->execute(array($_REQUEST['id']));
-	$total = $statement->rowCount();
-	if( $total == 0 ) {
-		header('location: logout.php');
-		exit;
-	}
+    // Check if the provided ID is valid
+    $statement = $pdo->prepare("SELECT * FROM tbl_service WHERE id=?");
+    $statement->execute(array($_REQUEST['id']));
+    $total = $statement->rowCount();
+    if($total == 0) {
+        // Redirect to logout if no record is found with the provided ID
+        header('location: logout.php');
+        exit;
+    }
 }
 ?>
 
 <?php
+// Get the photo filename associated with the record to be deleted
+$statement = $pdo->prepare("SELECT * FROM tbl_service WHERE id=?");
+$statement->execute(array($_REQUEST['id']));
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach ($result as $row) {
+    $photo = $row['photo'];
+}
 
-	// Getting photo ID to unlink from folder
-	$statement = $pdo->prepare("SELECT * FROM tbl_service WHERE id=?");
-	$statement->execute(array($_REQUEST['id']));
-	$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
-	foreach ($result as $row) {
-		$photo = $row['photo'];
-	}
+// Unlink (delete) the photo from the uploads folder
+if($photo != '') {
+    unlink('../assets/uploads/' . $photo);
+}
 
-	// Unlink the photo
-	if($photo!='') {
-		unlink('../assets/uploads/'.$photo);	
-	}
+// Delete the record from the tbl_service table
+$statement = $pdo->prepare("DELETE FROM tbl_service WHERE id=?");
+$statement->execute(array($_REQUEST['id']));
 
-	// Delete from tbl_service
-	$statement = $pdo->prepare("DELETE FROM tbl_service WHERE id=?");
-	$statement->execute(array($_REQUEST['id']));
-
-	header('location: service.php');
+// Redirect to the service list page after deletion
+header('location: service.php');
 ?>
+
+<?php require_once('footer.php'); ?>
